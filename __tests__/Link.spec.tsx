aaeaf4ceaf528ac9783f487ec85f75ede33854ff
Satoshi Iwao <stsiwo@gmail.com>
2018-12-09 11:25:52 -0800

    set up enzyme with jest

--- __tests__/Link.spec.tsx
+++ __tests__/Link.spec.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Link from '../src/Link';
 import * as renderer from 'react-test-renderer';
+import { shallow, mount, render } from 'enzyme';
 
 test('Link changes the class when hovered', () => {
   const component = renderer.create(
     <Link page="http://www.facebook.com">Facebook</Link>,
   );
+  const wrapper = shallow(<Link />);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 
