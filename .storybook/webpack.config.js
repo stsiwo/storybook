753372a0e1b5f22cdf9b2fe1bbf67d4a39d8f33d
Satoshi Iwao <stsiwo@gmail.com>
2018-12-10 17:11:56 -0800

    storybook webpack config to Extend Mode

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,52 @@
 const path = require('path');
-const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin');
-module.exports = (baseConfig, env, config) => {
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    loader: require.resolve('awesome-typescript-loader'),
-  });
-  config.plugins.push(new TSDocgenPlugin()); // optional
-  config.resolve.extensions.push('.ts', '.tsx');
-  return config;
-};
+//const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin');
+//module.exports = (baseConfig, env, config) => {
+  //config.module.rules.push({
+    //test: /\.(ts|tsx)$/,
+    //loader: require.resolve('awesome-typescript-loader'),
+  //});
+  //console.log(config.module.rules);
+  //config.plugins.push(new TSDocgenPlugin()); // optional
+  //config.resolve.extensions.push('.ts', '.tsx');
+  //return config;
+//};
+//
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        include: path.resolve(__dirname, '../src'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      { test: /\.tsx?$/, 
+        loader: "awesome-typescript-loader",
+        include: path.resolve(__dirname, '../src'),
+      },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      {
+        test: /\.css$/,
+        loaders: ["style-loader", "css-loader"],
+        include: path.resolve(__dirname, '../src'),
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        include: path.resolve(__dirname, '../src'),
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx']
+  }
+
+}
--- src/components/Icon/Icon.story.tsx
+++ src/components/Icon/Icon.story.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import Icon from './Icon';
-import svgIcon from './assets/new.svg';
-import json from './assets/myjson.json';
+//import svgIcon from './assets/new.svg';
+//import json from './assets/myjson.json';
+//const svgIcon = require('./assets/new.svg')
+import { svgIcon } from './assets/new.svg';
 
 storiesOf('Icon', module)
   .add('new', () => <Icon svgSrc={svgIcon}></Icon>)
